f9169ed765e9e237562ac30ced8c352c015c0e44
Toni Tassani <atassani@gmail.com>
2016-06-09 14:57:21 +0100

    Projects as array to simplify commenting out

--- jira_to_analytics_generate.sh
+++ jira_to_analytics_generate.sh
@@ -7,10 +7,13 @@ TODAY=`date +%Y-%m-%d`
 echo -n Password: 
 read -s password
 echo
-#for type in markingdom marketeers marketing_system itsquad; do
-#for type in markingdom marketeers itsquad; do
-for type in markingdom marketeers itsquad; do
-  echo "$password" | $JIRA_TO_ANALYTICS/jira_to_analytics.win64.exe -i $CONFIG/$type.yaml -o $RESULT/$type'_'$TODAY.csv
+projects=(
+"markingdom" 
+"marketeers" 
+"itsquad"
+);
+for project in ${projects[@]}; do
+  echo "$password" | $JIRA_TO_ANALYTICS/jira_to_analytics.win64.exe -i $CONFIG/$project.yaml -o $RESULT/$project'_'$TODAY.csv
 done
 
 
