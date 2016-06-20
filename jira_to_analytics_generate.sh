ff53a59063cccf67cc70e39100531de04e5d8fc4
Toni Tassani <atassani@gmail.com>
2016-06-20 16:04:41 +0100

    Variables can be overridden

--- jira_to_analytics_generate.sh
+++ jira_to_analytics_generate.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-JIRA_TO_ANALYTICS="C:\code\jira_to_analytics"
-CONFIG=$JIRA_TO_ANALYTICS/config
-RESULT=$JIRA_TO_ANALYTICS/data
+#
+# Extracts data from JIRA of different projects prepared for ActionableAgile.
+# By default expects the following extructure: 
+#  J2A_PATH/jira_to_analytics.exe
+#          /config/ all YAML files
+#          /data/   all CSV results
+#
+# The environment variables can be overridden.
+#
+J2A_PATH="${J2A_PATH:-C:\code\jira_to_analytics}"
+J2A_CONFIG="${J2A_CONFIG:-$J2A_PATH/config}"
+J2A_RESULT="${J2A_RESULT:-$J2A_PATH/data}"
 TODAY=`date +%Y-%m-%d`
 
 echo -n Password: 
@@ -13,7 +22,7 @@ projects=(
 "itsquad"
 );
 for project in ${projects[@]}; do
-  echo "$password" | $JIRA_TO_ANALYTICS/jira_to_analytics.win64.exe -i $CONFIG/$project.yaml -o $RESULT/$project'_'$TODAY.csv
+  echo "$password" | $J2A_PATH/jira_to_analytics.win64.exe -i $J2A_CONFIG/$project.yaml -o $J2A_RESULT/$project'_'$TODAY.csv
 done
 
 
